a2244fca82fd17e61fee31fd9ccb2e52a62ca1f0
thiyagarajakumar <thiyaguggggg0@gmail.com>
2026-09-09 17:51:29 +0530

    Solved LeetCode #15: 3Sum [Java]

---
+++ 17-Sorting/15-3sum.java
@@ -0,0 +1,13 @@
+// Problem Number: 15
+// Problem Name: 3Sum
+// Difficulty: Medium
+// Topic: Array, Sorting
+// Problem Link: https://leetcode.com/problems/3sum/
+// Time Complexity: O(1)
+// Space Complexity: O(1)
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        
+    }
+}
