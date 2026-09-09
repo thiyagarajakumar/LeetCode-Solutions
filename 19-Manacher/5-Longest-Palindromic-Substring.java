86e089c5f1eb46e4fd713883a3eab92d7bda1346
thiyagarajakumar <thiyaguggggg0@gmail.com>
2026-09-09 22:37:15 +0530

    Solved LeetCode #5: Longest Palindromic Substring [Java]

---
+++ 19-Manacher/5-Longest-Palindromic-Substring.java
@@ -0,0 +1,13 @@
+// Problem Number: 5
+// Problem Name: Longest Palindromic Substring
+// Difficulty: Medium
+// Topic: Two Pointers, Manacher
+// Problem Link: https://leetcode.com/problems/longest-palindromic-substring/
+// Time Complexity: O(1)
+// Space Complexity: O(1)
+
+class Solution {
+    public String longestPalindrome(String s) {
+        
+    }
+}
